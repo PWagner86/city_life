58a75d899d3e6f80e035b6c8cba317d088384caa
Peter Wagner <peter.wagner1986@gmail.com>
2024-10-02 16:47:09 +0200

    (feat, styling): added canvas and some formating

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 console.log('Hello City Life');
+
+const mainCanvas = document.querySelector('#main-canvas');
+const MAINCANVASWIDTH = mainCanvas.width = 1000;
+const MAINCANVASHEIGHT = mainCanvas.height = 600;
+const mainCtx = mainCanvas.getContext('2d');
